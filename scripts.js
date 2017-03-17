f4bb2906d2633b790b64e960f504922dcd92fe10
Noemi <andreoli.noemi@gmail.com>
2017-03-17 19:12:47 +0000

    done general structure. added some animations

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+$(document).ready(function(){
+  $(window).scroll(function(){
+    var distanceFromTop= $(this).scrollTop();
+    var orangeSection= $('.orange-section')[0];
+    var iconsFadeInPoint= orangeSection.offsetHeight + orangeSection.clientHeight;
+    if(distanceFromTop>iconsFadeInPoint){
+      $('.fa').addClass('pop-up');
+    }
+  })
+})

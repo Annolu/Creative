f67e66c78011dc00447045cc267205c8b09542e7
Noemi <andreoli.noemi@gmail.com>
2017-03-18 15:47:48 +0000

    smooth scroll to internal link

--- scripts.js
+++ scripts.js
@@ -39,4 +39,19 @@ $(document).ready(function(){
 
     iconsPupUp();
   })
+
+  //smooth scroll to internal anchor link
+
+    $('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top - 100
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	})
 })
